329467a7f3df34acae862c08c2e4cae0e9d4eaa2
Leonardo Bringas <leonardobringashello@gmail.com>
2026-08-20 18:43:39 -0300

    Fix comments and update static file configuration in server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// Importing the necessary modules for the server and automatically reloading the browser.
+// importing the necessary modules for the server and automatically reloading the browser.
 const express = require('express');
 const livereload = require('livereload');
 const app = express();
 
-// Configure the current folder as static and index2.html as the main file.
-app.use(express.static(__dirname, { index: 'index2.html' }));
+// configure the current folder as static and index.html as the main file.
+app.use(express.static(__dirname, { index: 'index.html' }));
 
-// Start the server on port 3000 and log the URL in the console.
+// start the server on port 3000 and log the URL in the console.
 app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
 
-// LiveReload, just like before.
+// liveReload, just like before.
 const lrserver = livereload.createServer();
 lrserver.watch(__dirname);
